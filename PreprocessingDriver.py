1cc1d92899e0e6eb6ac48c3a286a0bd885879ba9
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-11-14 13:25:22 -0800

    Edits

--- PreprocessingDriver.py
+++ PreprocessingDriver.py
@@ -1,10 +1,7 @@
 import TCSS555PreProcessing as lib
 import pandas as pd
 
-
-df = pd.read_csv('C:\\Users\\cressm\\Desktop\\TCSS 555\\deceptive-opinion.csv')  
-lib.processTextWithLemmanization(df, 'C:\\Users\\cressm\\Desktop\\TCSS 555\\deceptive-opinion_test1.csv')
-
-
-
-
+df = pd.read_csv(
+    'C:\\Users\\cressm\\Desktop\\TCSS 555\\deceptive-opinion.csv')
+lib.processTextWithLemmanization(df,
+                                 'C:\\Users\\cressm\\Desktop\\TCSS 555\\deceptive-opinion_test1.csv')
